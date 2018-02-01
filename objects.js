a8ff7b865a6baa2953213b28085a56f3819b8896
eyi1 <ej7729@gmail.com>
2018-02-01 01:42:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = new Object({artist: 'album'});
 
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.artist = song
+  playlist.artist = album
   return playlist
 }
