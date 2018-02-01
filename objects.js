a4f7c0d7a0f1604b27ed3ec552b7c42e57808334
eyi1 <ej7729@gmail.com>
2018-02-01 01:44:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = new Object({artist: 'album', artistName: 'Phil Ochs'});
 
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.artistName = songTitle
+  playlist[artistName] = songTitle
   return playlist
 }
